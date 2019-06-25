0bbc2327f562f49af0d6537b2db4d490f97112bd
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-06-25 13:40:51 +0200

    Added gather-all-variables function.

--- src/compiler/implementation.lisp
+++ src/compiler/implementation.lisp
@@ -1,7 +1,7 @@
 (cl:in-package #:huginn.compiler)
 
 
-(defun gather-all-sublists (list &aux
+(defun gather-all-sublists (list &key
                                    (index 0)
                                    (result (make-hash-table :test 'eq)))
   (labels ((impl (sublist)
@@ -13,3 +13,16 @@
                  (map nil #'impl sublist)))))
     (impl list)
     result))
+
+
+(defun gather-all-variables (list &key
+                                    (index 0)
+                                    (result (make-hash-table :test 'eq)))
+  (~> list flatten
+      (remove-if (lambda (x) (typep x 'huginn.m.r:word)) _)
+      (map nil (lambda (variable)
+                 (let ((new-index (ensure (gethash variable result)
+                                    index)))
+                   (when (eql new-index index)
+                     (incf index))))))
+  result)
