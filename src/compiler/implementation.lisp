a937eab1800bddb20832e28b6d7c59631b7320f5
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-06-27 10:04:31 +0200

    Added walk-expressions function.

--- src/compiler/implementation.lisp
+++ src/compiler/implementation.lisp
@@ -6,44 +6,74 @@
        (string= (symbol-name variable) "?")))
 
 
-(defun variable-p (variable)
+(defun variablep (variable)
   (and (symbolp variable)
        (char= #\? (aref (symbol-name variable) 0))))
 
 
-(defun expression-p (element)
+(defun expressionp (element)
   (consp element))
 
 
-(defun gather-all-sublists (list &key
-                                   (index 0)
-                                   (result (make-hash-table :test 'eq)))
-  (labels ((impl (sublist)
-             (when (expression-p sublist)
-               (let ((new-index (ensure (gethash sublist result)
-                                  index)))
-                 (when (eql new-index index)
-                   (incf index (+ 2 (length sublist))))
-                 (map nil #'impl sublist)))))
-    (impl list)
-    (values result index)))
+(defun inlined-fixnum-p (element)
+  (typep element 'huginn.m.r:word))
 
 
-(defun gather-all-variable-bindings
+(defun valuep (element)
+  (nor (expressionp element)
+       (variablep element)
+       (inlined-fixnump element)))
+
+
+(defun walk-expressions (expression
+                         &key
+                           (on-expression #'identity)
+                           (on-value #'identity)
+                           (on-variable #'identity)
+                           (on-inlined-fixnum #'identity))
+  (labels ((impl (elt parent)
+             (cond ((variablep elt)
+                    (funcall on-variable elt parent))
+                   ((valuep elt)
+                    (funcall on-value elt parent))
+                   ((inlined-fixnum-p elt)
+                    (funcall on-inlined-fixnum elt parent))
+                   ((expression-p elt)
+                    (funcall on-expression elt parent)
+                    (iterate
+                      (for sub in elt)
+                      (impl elt sub))))))
+    (impl expression)))
+
+
+(defun gather-all-expressions (list &key
+                                      (index 0)
+                                      (result (make-hash-table :test 'eq)))
+  (walk-expressions
+   list
+   :on-expression (lambda (sublist parent)
+                    (declare (ignore parent))
+                    (let ((new-index (ensure (gethash sublist result)
+                                       index)))
+                      (when (eql new-index index)
+                        (incf index (+ 2 (length sublist))))
+                      (map nil #'impl sublist))))
+  (values result index))
+
+
+(defun gather-all-values
     (list &key
             (index 0)
             (result (make-hash-table :test 'eql)))
-  (labels ((impl (x)
-             (cond ((or (variable-p x)
-                        (typep x 'huginn.m.r:word))
-                    nil)
-                   ((expression-p x)
-                    (map nil #'impl x))
-                   (t (let ((new-index (ensure (gethash x result)
-                                         index)))
-                        (when (eql new-index index)
-                          (incf index)))))))
-    (values result index)))
+  (walk-expressions
+   list
+   :on-value (lambda (x parent)
+               (declare (ignore parent))
+               (let ((new-index (ensure (gethash x result)
+                                  index)))
+                 (when (eql new-index index)
+                   (incf index)))))
+  (values result index))
 
 
 (defclass compilation-state (fundamental-compilation-state)
