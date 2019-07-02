1d7b222641d11a44761624de48c6a9336aaf0633
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-07-02 16:40:05 +0200

    Added todos for compiler input validation.

--- src/compiler/conditions.lisp
+++ src/compiler/conditions.lisp
@@ -1,1 +1,16 @@
 (cl:in-package #:huginn.compiler)
+
+
+(defun has-predicate-p (expression)
+  cl-ds.utils:todo)
+
+
+(defun goalp (expression)
+  cl-ds.utils:todo)
+
+#|
+
+* TODO Validated if predicate is present, signal condition if not.
+* TODO Validate if each goal is proper.
+
+|#
