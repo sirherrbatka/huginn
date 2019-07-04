0d575c501a196c81792ca3cf06ce3ee59162d4c4
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-07-04 14:41:48 +0200

    Added report functionso for new exceptions.

--- src/compiler/conditions.lisp
+++ src/compiler/conditions.lisp
@@ -9,12 +9,23 @@
   cl-ds.utils:todo)
 
 
-(define-condition invalid-goal (program-error)
-  ())
+(define-condition compiler-error (program-error)
+  ((%form :reader read-form
+          :initarg :form)))
+
+
+(define-condition invalid-goal (compiler-error)
+  ()
+  (:report (lambda (condition stream)
+             (format stream "Invalid goal in the form ~a."
+                     (read-form condition)))))
 
 
 (define-condition invalid-predicate (program-error)
-  ())
+  ()
+  (:report (lambda (condition stream)
+             (format stream "Invalid predicate ~a."
+                     (read-form condition)))))
 
 #|
 
