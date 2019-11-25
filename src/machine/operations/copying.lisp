b3d62b10b69bbb0df516b9e0c12281b6b9f01ae5
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-11-25 16:06:14 +0100

    Added stub for copy-recursive-head.

--- src/machine/operations/copying.lisp
+++ src/machine/operations/copying.lisp
@@ -245,8 +245,24 @@
             (huginn.m.r:dereference-heap-pointer state reference-pointer nil))))
 
 
-  (defun copy-recursive-head (state stack-cell)
-    cl-ds.utils:todo)
+  (-> copy-recursive-head (huginn.m.r:execution-state huginn.m.r:execution-stack-cell) t)
+  (defun copy-recursive-head (execution-state stack-cell)
+    (bind ((clause (huginn.m.r:execution-stack-cell-clause stack-cell))
+           (copy-head-function (huginn.m.r:clause-copy-head-function
+                                clause))
+           (heap-pointer (huginn.m.r:execution-stack-cell-heap-pointer
+                          stack-cell))
+           (head-length (huginn.m.r:clause-head-length clause))
+           (clause-length (huginn.m.r:clause-content-length clause))
+           (bindings-fill-pointer (huginn.m.r:execution-stack-cell-bindings-fill-pointer
+                                   stack-cell))
+           (recursive-head-pointer (+ (huginn.m.r:execution-stack-cell-heap-pointer
+                                       stack-cell)
+                                      clause-length)))
+      (declare (type huginn.m.r:pointer recursive-head-pointer))
+      (if (null copy-head-function)
+          cl-ds.utils:todo
+          cl-ds.utils:todo)))
 
 
   (-> copy-recursive-body (huginn.m.r:execution-state huginn.m.r:execution-stack-cell) t)
